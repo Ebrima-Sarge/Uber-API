7316278cbb0314a52c930de5ec00c0d92428e13e
Ebrima-Sarge <ebrimasarge21@gmail.com>
2026-02-03 13:54:37 +0100

    Api update

---
+++ riders.js
@@ -0,0 +1,154 @@
+const express = require('express');
+const router = express.Router();
+const app = express();
+app.use(express.json());
+const bodyParser = require('body-parser');
+
+const { MongoClient, ObjectId } = require("mongodb");
+
+const client = new MongoClient("mongodb://localhost:27017");
+
+async function getCollection() {
+   
+    await client.connect();
+    
+    const db = client.db("rides");
+    return db.collection("passengers");
+
+}
+const port = 5000;
+
+const passengers = [];
+
+
+app.post('/passenger', async (req, res) => {
+
+   try{
+   const collection = await getCollection();
+    const { Name, From, To } = req.body;
+
+    
+
+    if (!Name || !From || !To) {
+        return res.status(406).json({
+            message: 'passengers details required'
+        });
+    }
+
+    const newPassenger = {
+        Name,
+        From,
+        To,
+        Date: new Date(),
+        PassengerId: Date.now()
+    };
+
+    await collection.insertOne(newPassenger)
+    res.status(202).json(newPassenger)
+
+}catch (error) {console.error(error);
+    res.status(523).json({error: "Failed to save to database"})
+}
+});
+
+
+
+app.get('/passenger/:id', async (req, res) => {
+   try {
+    const collection = await getCollection();
+    const id = Number(req.params.id); 
+
+    const passenger = await collection.findOne({ PassengerId: id });
+
+    if (!passenger) {
+        return res.status(404).json({
+            message: 'Passenger not found'
+        });
+    } else {
+
+    res.status(201).json(passenger);
+    }
+ } catch (error) {
+        res.status(500).json({error: "server error"});
+    }
+
+});
+
+app.patch('/passenger/userName', async (req,res) => { 
+    
+    try {
+
+    const collection = await getCollection();
+    const {newFrom , newTo}= req.body;
+
+    const updatedPassenger =  await collection.updateOne( 
+        {"Name" : username},
+        {
+          $set: {
+            From: newFrom,
+            To: newTo,
+          }  
+        }
+        
+    );
+
+    if (updatedPassenger.matchedCount === 0) {
+        return res.status(404).json({message : "User not found to be deleted"});
+    }
+        res.status(200).json({message: "Ride is successfully updated"});
+
+} catch (error) {
+    res.send(500).json({error: "update failed"})
+}
+     
+
+});
+
+
+app.delete('/passenger/:id/:Uname', async (req, res) => { 
+    try {
+    const collection = await getCollection();
+    const id = Number(req.params.id);
+    const Uname = req.params.Uname;
+
+   
+const passengerTobeDeleted = await collection.deleteOne({PassenegrId: id, Name: Uname});
+
+
+
+if (passengerTobeDeleted.count === 0 ) { res.status(404).json({message: "user not found"});
+
+}
+
+        res.status(200).json({ message: "Passenger deleted successfully" });
+       
+    } catch (error) {
+        res.status(500).json({ error: "Server error during deletion" });
+    }
+});
+
+
+
+
+
+app.listen(port, () => {
+   console.log('listining on port' + port)
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
